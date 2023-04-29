baad73d9a012b9d6ae7265d3b04c2c7f118cc031
Evandro oliveira <vandoaparecido@hotmail.com>
2023-04-29 19:31:22 -0300

    :car: :taxi: :tractor: Fontes personalizadas + Componente de Loading

--- App.tsx
+++ App.tsx
@@ -1,7 +1,25 @@
 import { ThemeProvider } from "styled-components/native";
 import { SignIn } from "./src/screens/SignIn";
 import theme from "./src/theme";
+
+import { Loading } from "./src/components/Loading";
+
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <Loading />;
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <SignIn />
