55837b1b420010cd2e60476f53ecc67a51cd99d4
Evandro oliveira <vandoaparecido@hotmail.com>
2023-04-30 01:49:56 -0300

    :car: :taxi: :tractor: Buscando dados com fetch api

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Alert } from "react-native";
 import * as WebBrowser from "expo-web-browser";
 import * as Google from "expo-auth-session/providers/google";
 import { Container, Title, Slogan } from "./styles";
@@ -29,10 +30,17 @@ export function SignIn() {
   useEffect(() => {
     if (response?.type === "success") {
       if (response.authentication?.idToken) {
-        console.log(
-          "TOKEN DE AUTENTICAÇÃO =>",
-          response.authentication.idToken
+        fetch(
+          `https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=${response.authentication.idToken}`
+        )
+          .then((response) => response.json())
+          .then(console.log);
+      } else {
+        Alert.alert(
+          "Entrar",
+          "Não foi possível conectar-se a sua conta google."
         );
+        setIsAuthenticating(false);
       }
     }
   }, [response]);
