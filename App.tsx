047a6c0eaa7214a1cb614bd5bec0f5ff430bc9f3
Evandro oliveira <vandoaparecido@hotmail.com>
2023-05-01 00:14:14 -0300

    :car: :taxi: :tractor: Instalando e configurando o RelmDb

--- App.tsx
+++ App.tsx
@@ -3,12 +3,17 @@ import { ThemeProvider } from "styled-components/native";
 import { SignIn } from "./src/screens/SignIn";
 import theme from "./src/theme";
 import { Loading } from "./src/components/Loading";
+import { Home } from "./src/screens/Home";
+
+import { AppProvider, UserProvider } from "@realm/react";
 
 import {
   useFonts,
   Roboto_400Regular,
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
+
+import { REALM_APP_ID } from "@env";
 
 import { ANDROID_CLIENT_ID } from "@env";
 
@@ -23,13 +28,17 @@ export default function App() {
   }
 
   return (
-    <ThemeProvider theme={theme}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      <SignIn />
-    </ThemeProvider>
+    <AppProvider id={REALM_APP_ID}>
+      <ThemeProvider theme={theme}>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        <UserProvider fallback={SignIn}>
+          <Home />
+        </UserProvider>
+      </ThemeProvider>
+    </AppProvider>
   );
 }
