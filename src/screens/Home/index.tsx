bb48f7a03c89153617dee4045d57f258e149bbb8
Evandro oliveira <vandoaparecido@hotmail.com>
2023-05-01 00:46:13 -0300

    :car: :taxi: :tractor: Estrutura do header

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,5 +1,10 @@
+import { HomeHeader } from "../../components/HomeHeader";
 import { Container } from "./styles";
 
 export function Home() {
-  return <Container />;
+  return (
+    <Container>
+      <HomeHeader />
+    </Container>
+  );
 }
