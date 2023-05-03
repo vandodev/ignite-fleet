ed2e3aa1241feed13f4a9f566e02909db80fb3ba
vandodev <vandoaparecido@hotmail.com>
2023-05-03 09:44:21 -0300

    :car: :taxi: :tractor: Deslogando usuário

--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -2,12 +2,19 @@ import React from "react";
 import { TouchableOpacity } from "react-native";
 import { Power } from "phosphor-react-native";
 import { Container, Greeting, Message, Name , Picture} from "./styles";
-import { useUser } from '@realm/react';
+import { useUser, useApp  } from '@realm/react';
 
 import theme from "../../theme";
 
 export function HomeHeader() {
   const user = useUser();
+
+  const app = useApp();
+
+  function handleLogOut() {
+    app.currentUser?.logOut();
+  }
+
   return (
     <Container>
       <Picture 
@@ -19,7 +26,7 @@ export function HomeHeader() {
 
         <Name>{user?.profile.name}</Name>
       </Greeting>
-      <TouchableOpacity>
+      <TouchableOpacity activeOpacity={0.7} onPress={handleLogOut}>
         <Power size={32} color={theme.COLORS.GRAY_400} />
       </TouchableOpacity>
     </Container>
