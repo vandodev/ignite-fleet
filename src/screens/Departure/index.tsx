b2442847e3bae4bb6703f8454c96bada59badfbb
vandodev <vandoaparecido@hotmail.com>
2023-05-05 13:29:29 -0300

    :car: :taxi: :tractor: Criando o TextAreaInput

--- src/screens/Departure/index.tsx
+++ src/screens/Departure/index.tsx
@@ -1,6 +1,7 @@
 import { Header } from '../../components/Header';
 import { LicensePlateInput } from '../../components/LicensePlateInput';
 import { Container, Content } from './styles';
+import { TextAreaInput } from '../../components/TextAreaInput';
 
 export function Departure() {
   return (
@@ -11,6 +12,11 @@ export function Departure() {
         <LicensePlateInput 
           label='Placa do veículo' 
           placeholder="BRA1234"
+        />
+
+        <TextAreaInput 
+          label='Finalizade'
+          placeholder='Vou utilizar o veículo para...'
         />
       </Content>
 
