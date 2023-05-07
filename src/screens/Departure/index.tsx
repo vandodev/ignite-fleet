5001a6b4878f6bcf37dd04a39c58631c90edd99a
vandodev <vandoaparecido@hotmail.com>
2023-05-07 10:17:16 -0300

    :car: :taxi: :tractor:Adicionando ScrollView e KeyboardAvoidingView

--- src/screens/Departure/index.tsx
+++ src/screens/Departure/index.tsx
@@ -1,10 +1,12 @@
 import { useRef } from 'react';
-import { TextInput } from 'react-native';
+import { TextInput, ScrollView, KeyboardAvoidingView, Platform } from 'react-native';
 import { Header } from '../../components/Header';
 import { LicensePlateInput } from '../../components/LicensePlateInput';
 import { Container, Content } from './styles';
 import { TextAreaInput } from '../../components/TextAreaInput';
 import { Button } from '../../components/Button';
+
+const keyboardAvoidingViewBehavior = Platform.OS === 'android' ? 'height' : 'position';
 
 export function Departure() {
 
@@ -18,28 +20,34 @@ export function Departure() {
     <Container>
       <Header title='Saida' />
 
-      <Content>
-        <LicensePlateInput 
-          label='Placa do veículo' 
-          placeholder="BRA1234"
-          onSubmitEditing={() => {
-            descriptionRef.current?.focus()
-          }}
-          returnKeyType='next'
-        />
+          <KeyboardAvoidingView style={{ flex: 1 }} behavior={keyboardAvoidingViewBehavior} >
+        <ScrollView>
+          <Content>
+            <LicensePlateInput
+              label='Placa do veículo'
+              placeholder="BRA1234"
+              onSubmitEditing={() => {
+                descriptionRef.current?.focus()
+              }}
+              returnKeyType='next'
+            />
 
-        <TextAreaInput 
-          ref={descriptionRef}
-          label='Finalizade'
-          placeholder='Vou utilizar o veículo para...'
-          onSubmitEditing={handleDepartureRegister}
-          returnKeyType='send'
-          blurOnSubmit
+            <TextAreaInput
+              ref={descriptionRef}
+              label='Finalizade'
+              placeholder='Vou utilizar o veículo para...'
+              onSubmitEditing={handleDepartureRegister}
+              returnKeyType='send'
+              blurOnSubmit
+            />
 
-        />
-
-        <Button title='Registrar saida'  onPress={handleDepartureRegister}/>
-      </Content>
+            <Button 
+              title='Registar Saída'
+              onPress={handleDepartureRegister}
+            />
+          </Content>
+        </ScrollView>
+      </KeyboardAvoidingView>
 
     </Container>
   );
