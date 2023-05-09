ac0456b3cd50d0cabf2b8d8b7291fe6ecb9bba15
vandodev <vandoaparecido@hotmail.com>
2023-05-09 09:31:46 -0300

    :car: :taxi: :tractor: Buscando todos os dados cadastrado

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,14 +1,26 @@
+import { useEffect } from 'react';
 import { useNavigation } from '@react-navigation/native';
+import { useQuery } from '../../libs/realm';
+import { Historic } from '../../libs/realm/schemas/Historic';
 import { CarStatus } from "../../components/CarStatus";
 import { HomeHeader } from "../../components/HomeHeader";
 import { Container, Content } from "./styles";
 
 export function Home() {
   const { navigate } = useNavigation();
+  const historic = useQuery(Historic);
 
   function handleRegisterMoviment() {
     navigate('departure')
   }
+
+  function fetchVehicle() {
+    console.log(historic);
+  }
+
+  useEffect(() => {
+    fetchVehicle();
+  },[])
 
   return (
     <Container>
