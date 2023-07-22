70813db3c648b0828012ab9442b7d879b98ccef7
vandodev <vandoaparecido@hotmail.com>
2023-07-22 07:49:33 -0300

    :car: :taxi: :tractor: Salvando as coordenadas no storage

--- src/libs/asyncStorage/locationStorage.ts
+++ src/libs/asyncStorage/locationStorage.ts
@@ -2,6 +2,12 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const STORAGE_KEY = '@ignitefleet:location';
 
+type LocationProps = {
+    latitude: number;
+    longitude: number;
+    timestamp: number;
+  }
+
 export async function getStorageLocation() {
   const storage = await AsyncStorage.getItem(STORAGE_KEY)
 
@@ -9,3 +15,11 @@ export async function getStorageLocation() {
 
   return response
 }
+
+export async function saveStorageLocation(newLocation: LocationProps) {
+    const storage = await getStorageLocation()
+  
+    storage.push(newLocation)
+  
+    await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(storage))
+  }
