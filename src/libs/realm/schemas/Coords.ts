0c57f053410e4bef07f0423190cc4a94f5f70cce
vandodev <vandoaparecido@hotmail.com>
2023-07-25 09:06:31 -0300

    :car: :taxi: :tractor: Criando o Schema de Coordenadas

---
+++ src/libs/realm/schemas/Coords.ts
@@ -0,0 +1,35 @@
+// Acesse o atlas -> app services -> schema (observe que o schema não alterou)
+// Acesse Device sync -> clique em terminate sync
+// Habilite o Development Mode
+
+import { Realm } from '@realm/react';
+
+export type CoordsSchemaProps = {
+  latitude: number;
+  longitude: number;
+  timestamp: number;
+}
+
+export class Coords extends Realm.Object<Coords> {
+  latitude!: number;
+  longitude!: number;
+  timestamp!: number;
+
+  static generate({ latitude, longitude, timestamp }: CoordsSchemaProps) {
+    return {
+      latitude,
+      longitude,
+      timestamp
+    }
+  }
+
+  static schema = {
+    name: 'Coords',
+    embedded: true,
+    properties: {
+      latitude: 'float',
+      longitude: 'float',
+      timestamp: 'float'
+    }
+  }
+}
